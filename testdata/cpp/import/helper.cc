a3763585a9e3cb63049df22f18552bb7f0cf6bbb
Julian Vossen <julian.vossen@rwth-aachen.de>
2017-12-08 23:57:56 +0000

    Add test data for includes in C++

---
+++ testdata/cpp/import/helper.cc
@@ -0,0 +1,5 @@
+#include "helper.h"
+#include <cstring>
+
+void help() {
+}
---
+++ testdata/cpp/import/main.cpp
@@ -0,0 +1,10 @@
+#include "helper.h"
+#include "subdir/subhelper.h"
+#include"remote.h"
+
+#include <iostream>
+#include<vector>
+
+int main() {
+    std::cout << "main" << std::endl;
+}
