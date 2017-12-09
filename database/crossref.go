40c923d665aa60aef654462944e08af396424b5f
Julian Vossen <julian.vossen@rwth-aachen.de>
2017-12-09 17:51:02 +0000

    Implement GetImportedFiles

--- database/crossref.go
+++ database/crossref.go
@@ -1,9 +1,41 @@
 package database
 
-import ()
+import (
+    "path"
+    "strings"
+    "errors"
+)
 
-func (db *Database) GetImportedFiles(ref ImportRef) []File {
-	return []File{}
+func (db *Database) getCppImportedFiles(ref ImportRef) ([]File, error) {
+    files := []File{}
+    for filepath, file := range (*db).files {
+        switch {
+        case filepath == path.Join(ref.dir, ref.symbol):
+            return []File{ file }, nil
+        case strings.HasSuffix(filepath, ref.symbol):
+            files = append(files, file)
+        }
+    }
+    if len(files) <= 1 {
+        return files, nil
+    }
+    return files, errors.New("Ambiguous import " + ref.symbol + " in " +
+        ref.filename)
+}
+
+func (db *Database) GetImportedFiles(ref ImportRef) ([]File, error) {
+        if ref.type_ == SYSTEM {
+            return []File{}, nil
+        }
+        switch ref.language {
+        case CPP:
+            return (*db).getCppImportedFiles(ref)
+        }
+	return []File{}, nil
+}
+
+func (db *Database) GetImports() []ImportRef {
+        return (*db).imports
 }
 
 func (db *Database) GetCaller(ref FuncCallRef) FuncRef {
