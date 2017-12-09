34304e41598dbb4c9ee09ba4e6223d51a95826f4
Julian Vossen <julian.vossen@rwth-aachen.de>
2017-12-09 17:52:38 +0000

    Add simple dependency graph implementation

---
+++ graph/dependency.go
@@ -0,0 +1,38 @@
+package graph
+
+import (
+    "path"
+    db "github.com/codeplots/src2dot/database"
+)
+
+func DependencyGraph(store db.Database) (Graph, error) {
+    g := Graph {
+        nodes: []Node{},
+        edges: []Edge{},
+        isDirected: true,
+    }
+
+    for _, ref := range store.GetImports() {
+        id := path.Join(ref.Dir(), ref.Filename())
+        g.addNodeIfNotExist(Node{
+            id: id,
+            label: ref.Filename(),
+            parentId: ref.Dir(),
+        })
+
+        files, _ := store.GetImportedFiles(ref)
+        for _, file := range files {
+            fileId := path.Join(file.Dir(), file.Name())
+            g.addNodeIfNotExist(Node{
+                id: fileId,
+                label: file.Name(),
+                parentId: file.Dir(),
+            })
+            g.edges = append(g.edges, Edge{
+                sourceId: id,
+                targetId: fileId,
+            })
+        }
+    }
+    return g, nil
+}
