0986a491cb5dc0a1b4954d9f0fef59884b71d3ac
Julian Vossen <julian.vossen@rwth-aachen.de>
2017-12-10 13:05:49 +0000

    Move main into cmd folder

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,22 @@
 package main
 
-import "flag"
+import (
+        "flag"
+	"fmt"
+	db "github.com/codeplots/src2dot/database"
+        "github.com/codeplots/src2dot/graph"
+)
+
+func main() {
+	opts := parseOptions()
+	fmt.Println("Options: ", opts)
+	// cscope -kbc
+	// ctags --all-kinds=* --fields=* -R --extras=+frF .
+	myDb := db.FromCtags(opts.ctags)
+	myDb.AddCscope(opts.cscope)
+        g, _ := graph.DependencyGraph(myDb)
+        fmt.Println(g.ToDot())
+}
 
 type Options struct {
 	ctags  string
