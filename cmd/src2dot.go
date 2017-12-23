28fa4a9e1e0d0659dddef715375c9a10a0c0d304
Julian Vossen <julian.vossen@rwth-aachen.de>
2017-12-23 12:57:02 +0100

    Implement CLI

--- cmd/src2dot.go
+++ cmd/src2dot.go
@@ -3,27 +3,58 @@ package main
 import (
         "flag"
 	"fmt"
+        "io/ioutil"
+        "os"
 	db "github.com/codeplots/src2dot/database"
         "github.com/codeplots/src2dot/graph"
 )
 
+const (
+    DEP_FILENAME        string = "dependency_graph.dot"
+    CLASS_FILENAME      string = "class_diagram.dot"
+    CALL_FILENAME       string = "call_graph.dot"
+    PERMISSION          os.FileMode    = 0644
+)
+
 func main() {
 	opts := parseOptions()
-	// cscope -kbc
-        // pycscope -R
-        // starscope -e cscope
-	// ctags --all-kinds=* --fields=* -R --extras=+frF .
-	myDb := db.FromCtags(opts.ctags)
-	myDb.AddCscope(opts.cscope)
-        g, _ := graph.DependencyGraph(myDb)
-        fmt.Println(g.ToDot())
+
+	store := db.FromCtags(opts.ctags)
+	store.AddCscope(opts.cscope)
+
+        if !(opts.dep || opts.class || opts.call) {
+            dep, _ := graph.DependencyGraph(store)
+            err := ioutil.WriteFile(DEP_FILENAME, []byte(dep.ToDot()), PERMISSION)
+            if err != nil {
+                panic(err)
+            }
+            class, _ := graph.ClassDiagram(store)
+            err = ioutil.WriteFile(CLASS_FILENAME, []byte(class.ToDot()), PERMISSION)
+            if err != nil {
+                panic(err)
+            }
+
+            return
+        }
+
+        if opts.dep {
+            dep, _ := graph.DependencyGraph(store)
+            fmt.Println(dep.ToDot())
+        }
+        if opts.class {
+            class, _ := graph.ClassDiagram(store)
+            fmt.Println(class.ToDot())
+        }
+
 }
 
 type Options struct {
 	ctags  string
 	cscope string
 	raw    bool
-	o      string
+	dep      bool
+	class      bool
+	call      bool
 }
 
 func parseOptions() Options {
@@ -32,8 +63,12 @@ func parseOptions() Options {
 		"path to ctags output file")
 	flag.StringVar(&opts.cscope, "cscope", "./cscope.out",
 		"path to cscope output file")
-	flag.StringVar(&opts.o, "o", "-",
-		"output file (- for stdout)")
+	flag.BoolVar(&opts.dep, "dep", false,
+		"print dependency graph to stdout (suppresses all output to files)")
+	flag.BoolVar(&opts.class, "class", false,
+		"print class diagram to stdout (suppresses all output to files)")
+	flag.BoolVar(&opts.call, "call", false,
+		"print call graph to stdout (suppresses all output to files)")
 	flag.BoolVar(&opts.raw, "raw", false,
 		"output raw json graph")
 	flag.Parse()
